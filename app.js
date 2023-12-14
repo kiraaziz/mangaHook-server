f5d4ad041f73dbce47ed4d15d0044931e2a0965c
kiraaziz <kiraaziz2020@gmail.com>
2023-12-14 16:42:39 +0100

    v0.2

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = require("express")()
 const bodyParser = require("body-parser")
 const mangaRouter = require("./routes/mangaRouter")
 const mangaListRouter = require("./routes/mangaListRouter")
+const mangaSearch = require("./routes/mangaSearch")
 
 app.use(bodyParser.json())
 require('dotenv').config()
@@ -10,6 +11,7 @@ require('dotenv').config()
 app.use(ApiKey)
 app.use("/api/manga", mangaRouter)
 app.use("/api/mangaList", mangaListRouter)
+app.use("/api/search", mangaSearch)
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Server Start On Port ${process.env.PORT} 🎉✨ `)
